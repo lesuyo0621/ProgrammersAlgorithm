61549f6cd13c812c436da438508f41420869736c
lesuyo0621 <lesuyo930621@naver.com>
2020-06-01 03:52:26 +0900

    java level3 network add

---
+++ java/level3/network.java
@@ -0,0 +1,35 @@
+public class network {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        int[] check = new int[n];
+
+        for (int i = 0; i < check.length; i++) {
+            if (check[i] == 0) {
+                find(i, computers, check);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public void find(int nodeNum, int[][] computers, int[] check) {
+
+        check[nodeNum] = 1;
+        for (int j = 0; j < computers[nodeNum].length; j++) {
+            if (computers[nodeNum][j] == 1 && check[j] == 0) {
+                find(j, computers, check);
+            }
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+        int[][] computers = { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } };
+
+        network nt = new network();
+        System.out.println(nt.solution(n, computers));
+    }
+}
