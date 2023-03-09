de21116934079b0cd3d1fe092b5c662e2faaafca
lesuyo0621 <lesuyo930621@naver.com>
2023-03-10 00:50:00 +0900

    feat : Subtract / level0 두 수의 차 추가

---
+++ java/level0/Subtract.java
@@ -0,0 +1,13 @@
+public class Subtract {
+
+    private static int solution(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(solution(num1, num2));
+    }
+}
