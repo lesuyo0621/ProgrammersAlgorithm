b7d70139a8874a04176a4ff4937a108b9ae9f81b
lesuyo0621 <lesuyo930621@naver.com>
2023-03-10 00:54:05 +0900

    feat : Share / level0 몫 구하기

---
+++ java/level0/Share.java
@@ -0,0 +1,13 @@
+public class Share {
+
+    private static int solution(int num1, int num2){
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println(solution(num1, num2));
+    }
+}
