466e48e3b1716225797707e720f8ac0767abdd07
lesuyo0621 <lesuyo930621@naver.com>
2023-03-13 23:46:08 +0900

    feat : Multiple / level 0 두 수의 곱 추가

---
+++ java/level0/Multiple.java
@@ -0,0 +1,13 @@
+public class Multiple {
+
+    private static int solution(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+
+        System.out.println(solution(num1, num2));
+    }
+}
