4ec58e3f63dd612db15d0693ffb0d6c49a0f2e60
lesuyo0621 <lesuyo930621@naver.com>
2023-03-19 00:02:08 +0900

    feat : PrintAge / level 0 나이 출력 추가

---
+++ java/level0/PrintAge.java
@@ -0,0 +1,11 @@
+public class PrintAge {
+
+    private static int solution(int age){
+        return 2023 - age;
+    }
+
+    public static void main(String[] args) {
+        int age = 40;
+        System.out.println(solution(age));
+    }
+}
