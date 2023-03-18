fc75bdc55b5a5941250f747e48572919a6ae1f90
lesuyo0621 <lesuyo930621@naver.com>
2023-03-19 00:04:30 +0900

    feat : Sum / level 0 두 수의 합 추가

---
+++ java/level0/Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    private static int solution(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(solution(num1, num2));
+    }
+}
