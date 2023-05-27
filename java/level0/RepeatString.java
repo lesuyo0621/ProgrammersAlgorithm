abcaf932c7e1ef5a85a4ad21ee1802bd6e8a8d48
lesuyo0621 <lesuyo930621@naver.com>
2023-05-28 01:49:24 +0900

    feat : RepeatString / level 0 문자열 곱하기 추가

---
+++ java/level0/RepeatString.java
@@ -0,0 +1,8 @@
+public class RepeatString {
+    public static void main(String[] args) {
+        String my_string = "love";
+        int k = 10;
+
+        System.out.println(my_string.repeat(k));
+    }
+}
