41d53ecc48e84d235afb2afbe726cc396e7aeb05
lesuyo0621 <lesuyo930621@naver.com>
2023-05-30 22:33:15 +0900

    feat : MultipleOfNM / level 0 공배수 추가

---
+++ java/level0/MultipleOfNM.java
@@ -0,0 +1,9 @@
+public class MultipleOfNM {
+    public static void main(String[] args) {
+        int number = 60;
+        int n = 2;
+        int m = 3;
+
+        System.out.println(number % n == 0 && number % m == 0 ? 1 : 0);
+    }
+}
