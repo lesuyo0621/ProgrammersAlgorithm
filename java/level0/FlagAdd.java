077cddb7e5215d7fe5b9ce6bb669b15e83e93de1
lesuyo0621 <lesuyo930621@naver.com>
2023-05-30 23:11:39 +0900

    feat : FlagAdd / level 0 flag 에 따라 다른 값 반환하기 추가

---
+++ java/level0/FlagAdd.java
@@ -0,0 +1,9 @@
+public class FlagAdd {
+    public static void main(String[] args) {
+        boolean flag = true;
+        int a = -4;
+        int b = 7;
+
+        System.out.println(flag ? a + b : a - b);
+    }
+}
