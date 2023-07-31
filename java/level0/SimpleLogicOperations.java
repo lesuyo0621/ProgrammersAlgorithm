0bb8771671045da5ee08390084cac7fbab21f98a
lesuyo0621 <lesuyo930621@naver.com>
2023-08-01 00:11:08 +0900

    feat : SimpleLogicOperations / level 0 간단한 논리 연산 추가

---
+++ java/level0/SimpleLogicOperations.java
@@ -0,0 +1,9 @@
+public class SimpleLogicOperations {
+    private static boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        return (x1 || x2) && (x3 || x4);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(false, true, true, true));
+    }
+}
