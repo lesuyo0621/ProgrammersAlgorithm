36e19e6b30f902e815adc1171491fa462e647fbc
lesuyo0621 <lesuyo930621@naver.com>
2023-08-26 19:27:09 +0900

    feat : ReverseString / level 0 문자열 뒤집기 추가

---
+++ kotlin/level0/ReverseString.kt
@@ -0,0 +1,7 @@
+class ReverseString {
+    companion object {
+        fun solution(my_string: String): String = my_string.reversed()
+    }
+}
+
+fun main() = println(ReverseString.solution("jaron"))
