d7de64403d8ba424efb557b3cd1862e5b4a3c07b
lesuyo0621 <lesuyo930621@naver.com>
2024-05-11 19:48:56 +0900

    feat : ChangeUpperCase / level 0 대문자로 바꾸기 추가

---
+++ java/level0/ChangeUpperCase.java
@@ -0,0 +1,9 @@
+public class ChangeUpperCase {
+    public static String solution(String myString){
+        return myString.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aBcDeFg"));
+    }
+}
