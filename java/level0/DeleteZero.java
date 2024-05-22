0b8b88fa21b9830b34dd76f7de6660d69c7703f1
lesuyo0621 <lesuyo930621@naver.com>
2024-05-23 00:10:35 +0900

    feat : DeleteZero / level 0 0 떼기 추가

---
+++ java/level0/DeleteZero.java
@@ -0,0 +1,9 @@
+public class DeleteZero {
+    private static String solution(String n_str) {
+        return String.valueOf(Integer.parseInt(n_str));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("0010"));
+    }
+}
