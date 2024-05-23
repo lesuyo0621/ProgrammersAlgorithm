4027bbacd6db647f622dc79bcc95166bec2d2db7
lesuyo0621 <lesuyo930621@naver.com>
2024-05-24 04:05:16 +0900

    feat : StringToInt / level 0 문자열을 정수로 변환하기 추가

---
+++ java/level0/StringToInt.java
@@ -0,0 +1,9 @@
+public class StringToInt {
+    private static int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("10"));
+    }
+}
