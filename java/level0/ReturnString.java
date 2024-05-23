aea23d241d8307b9bd4dffccccb5742b8f161de3
lesuyo0621 <lesuyo930621@naver.com>
2024-05-24 00:56:42 +0900

    feat : ReturnString / level 0 문자열로 반환 추가

---
+++ java/level0/ReturnString.java
@@ -0,0 +1,9 @@
+public class ReturnString {
+    private static String solution(int n) {
+        return String.valueOf(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+    }
+}
