af6e2f7a255b550c2f0a5727ce17fb086ab7c024
lesuyo0621 <lesuyo930621@naver.com>
2024-05-27 00:04:00 +0900

    feat : IntegerPart / level 0 정수 부분 추가

---
+++ java/level0/IntegerPart.java
@@ -0,0 +1,9 @@
+public class IntegerPart {
+    private static int solution(double flo) {
+        return (int) flo;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(1.42));
+    }
+}
