a730a395b0b18df5252455f30d6b9a2324d50a30
lesuyo0621 <lesuyo930621@naver.com>
2024-05-27 00:33:58 +0900

    feat : ToLower / level 0 소문자로 바꾸기 추가

---
+++ java/level0/ToLower.java
@@ -0,0 +1,9 @@
+public class ToLower {
+    private static String solution(String myString) {
+        return myString.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aBcDeFg"));
+    }
+}
