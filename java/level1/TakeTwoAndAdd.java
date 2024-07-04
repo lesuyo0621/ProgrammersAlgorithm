510433efa53d4b028acf14cc0553e9d0373a8003
lesuyo0621 <lesuyo930621@naver.com>
2024-07-05 04:28:02 +0900

    feat : BannedUser / level 1 두 개 뽑아서 더하기 추가
    - https://school.programmers.co.kr/learn/courses/30/lessons/68644

---
+++ java/level1/TakeTwoAndAdd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TakeTwoAndAdd {
+    private static Integer[] solution(int[] numbers) {
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.toArray(new Integer[0]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{2, 1, 3, 4, 1})));
+    }
+}
